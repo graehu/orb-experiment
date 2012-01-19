1a3670fc420d730ed74fee68530ac6decb65d60c
Aaron <almax27@hotmail.co.uk>
2012-01-19 21:05:03 +0000

    Implemented bare-bones test state

---
+++ src/GameStates/TestGameState.java
@@ -0,0 +1,39 @@
+package GameStates;
+
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.BasicGameState;
+import org.newdawn.slick.state.StateBasedGame;
+
+public class TestGameState extends BasicGameState 
+{
+
+	@Override
+	public void init(GameContainer _gc, StateBasedGame _sbg)
+			throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void render(GameContainer _gc, StateBasedGame _sbg, Graphics _g)
+			throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(GameContainer _gc, StateBasedGame _sbg, int _dt)
+			throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int getID() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,12 +1,15 @@
 
-package main;
+package Main;
 
 import java.io.FileNotFoundException;
 import java.io.File;
 import java.io.PrintStream;
 
 import org.newdawn.slick.*;
+import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
+
+import GameStates.TestGameState;
 
 public class Main extends StateBasedGame
 {
@@ -27,8 +30,8 @@ public class Main extends StateBasedGame
         {            
             AppGameContainer app = new AppGameContainer(new Main());
             //app.setIcon("icon.png");
-            app.setDisplayMode(1280, 800, false);
-            app.setShowFPS(false);
+            app.setDisplayMode(640, 480, false);
+            app.setShowFPS(true);
             app.setVSync(true);
             //app.setTargetFrameRate(60);
             app.start();
@@ -40,10 +43,15 @@ public class Main extends StateBasedGame
         
     }
 
+    BasicGameState mTestGameState;
+    
 	@Override
 	public void initStatesList(GameContainer _gc) throws SlickException 
 	{
 		// TODO Auto-generated method stub
+		mTestGameState = new TestGameState();
+		addState(mTestGameState);
 		
+		enterState(0);
 	}
 }
